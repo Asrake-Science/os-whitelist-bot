d2d24b183be29df82ccd81a8acac7dc265417af6
supex <lilsnippreal@gmail.com>
2023-01-10 18:34:18 +0100

    dont work

--- startWL_Bot.js
+++
@@ -1,4 +0,0 @@
-const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager('./D_JS-Node/WLBOT.js', { token: `INSERT_TOKEN_HERE` });
-manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
-manager.spawn();
